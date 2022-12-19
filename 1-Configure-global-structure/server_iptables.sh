97d4b40b2bef7b18aa57dc94d52cadd0a234d024
acamher <acamher7@gmail.com>
2022-12-19 12:24:36 +0100

    basic server routing

--- 1-Configure-global-structure/server_iptables.sh
+++ 1-Configure-global-structure/server_iptables.sh
@@ -5,3 +5,7 @@ interface_server=enx000ec6c09235
 # Configure interface
 ifconfig $interface_server 192.168.0.2 netmask 255.255.255.0
 route add default gw 192.168.0.1 $interface_server
+
+# IpTables
+iptables -t nat -F
+iptables -t nat -A POSTROUTING -o $interface_server -j MASQUERADE
