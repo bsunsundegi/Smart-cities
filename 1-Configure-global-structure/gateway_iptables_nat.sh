47f2dda8652a9b2215daa054cdf948fec2905af9
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2022-12-19 11:14:26 +0100

    Update gateway_iptables_nat.sh

--- 1-Configure-global-structure/gateway_iptables_nat.sh
+++ 1-Configure-global-structure/gateway_iptables_nat.sh
@@ -1,5 +1,11 @@
-ifconfig enx000ec6c09237 192.168.0.1 netmask 255.255.255.0
+# Configure IPs of the USB adapters
+ifconfig enx000ec6c091fd 192.168.0.1 netmask 255.255.255.0
+ifconfig enx000ec6c09233 192.168.1.1 netmask 255.255.255.0
+ifconfig enx000ec6c09237 192.168.2.1 netmask 255.255.255.0
+ifconfig wlan0 192.168.3.1 netmask 255.255.255.0
 
+# Enable IP forwarding
 echo "1" > /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+# Redirect all traffic to PC server
+iptables -t nat -A POSTROUTING -o enx000ec6c091fd -j MASQUERADE
