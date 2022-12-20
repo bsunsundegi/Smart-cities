2785e9f88e7e64e1844562e748cc48644ea0283f
Jose Mari Muguruza <78322990+jmuguruza@users.noreply.github.com>
2022-12-20 12:59:59 +0100

    Update server_iptables.sh

--- 1-Configure-global-structure/server_iptables.sh
+++ 1-Configure-global-structure/server_iptables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-interface_server=enx000ec6c09235
+interface_server = enx000ec6c09235
 
 # Configure interface
 ifconfig $interface_server 192.168.0.2 netmask 255.255.255.0
 route add default gw 192.168.0.1 $interface_server
 
-# IpTables
+# Iptables
 iptables -t nat -F
 iptables -t nat -A POSTROUTING -o $interface_server -j MASQUERADE
