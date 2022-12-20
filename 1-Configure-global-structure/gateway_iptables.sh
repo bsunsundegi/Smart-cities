e29377f97a8e07dc6624b735ed23df44ca1e4755
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2022-12-20 13:21:00 +0100

    Update gateway_iptables.sh

--- 1-Configure-global-structure/gateway_iptables.sh
+++ 1-Configure-global-structure/gateway_iptables.sh
@@ -1,22 +1,29 @@
+# Define variables with the name of the interfaces
+interface_server=enx000ec6c091fd
+interface_wlan=wlxe894f60b829a
+interface_internet=enp0s3
+
 # Configure IPs of the USB adapters
-ifconfig enx000ec6c091fd 192.168.0.1 netmask 255.255.255.0
+ifconfig $interface_server 192.168.0.1 netmask 255.255.255.0
 ifconfig enx000ec6c09233 192.168.1.1 netmask 255.255.255.0
 ifconfig enx000ec6c09237 192.168.2.1 netmask 255.255.255.0
-ifconfig wlan 192.168.3.1 netmask 255.255.255.0
-ifconfig enp0s3 192.168.4.1 netmask 255.255.255.0
+ifconfig $interface_wlan 192.168.3.1 netmask 255.255.255.0
+ifconfig $interface_internet 192.168.4.1 netmask 255.255.255.0
 
 # Enable IP forwarding
 echo "1" > /proc/sys/net/ipv4/ip_forward
+
+# Delete default routes
+ip route flush all
+
+# Configure routes
+route add default gw 192.168.4.1 $interface_internet
+ip route add 192.168.0.2 dev $interface_server
 
 # Flush iptables
 iptables -t nat -F
 iptables -t filter -F
 iptables -t mangle -F
 
-# Configure the enp0s3 interface as default gateway
-route add default gw 192.168.4.1 enp0s3
-
-# The route to the server is not configured because it is automaticaly added when configuring the interface
-
 # Apply NAT to the traffic of the user to Internet
-iptables -t nat -A POSTROUTING -i wlan -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $interface_internet -j MASQUERADE
