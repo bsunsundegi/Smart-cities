4be14e5412d29a14d692c386b1bac6a41d484740
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2022-12-23 11:50:47 +0100

    Update server_iptables.sh

--- 1-Configure-global-structure/server_iptables.sh
+++ 1-Configure-global-structure/server_iptables.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+# Define variables with the name of the interfaces
+interface_gateway=enx000ec6c09235
 
-interface_server=enx000ec6c09235
+# Configure IP address of the interfaces
+ifconfig $interface_gateway 192.168.0.2 netmask 255.255.255.0
 
-# Configure interface
-ifconfig $interface_server 192.168.0.2 netmask 255.255.255.0
 route add default gw 192.168.0.1 $interface_server
-
-# Iptables
-iptables -t nat -F
-iptables -t nat -A POSTROUTING -o $interface_server -j MASQUERADE
