166130b573f604cee8bbb05163e7707eb50fed34
Jose Mari Muguruza <78322990+jmuguruza@users.noreply.github.com>
2022-12-26 10:15:40 +0100

    Update server_iptables.sh

--- 1-Configure-global-structure/server_iptables.sh
+++ 1-Configure-global-structure/server_iptables.sh
@@ -4,4 +4,4 @@ interface_gateway=enx000ec6c09235
 # Configure IP address of the interfaces
 ifconfig $interface_gateway 192.168.0.2 netmask 255.255.255.0
 
-route add default gw 192.168.0.1 $interface_server
+route add default gw 192.168.0.1 $interface_gateway
