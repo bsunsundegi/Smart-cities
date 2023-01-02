a4785915fbffb575bd5c5a832141cf1ef896759d
Jose Mari Muguruza <78322990+jmuguruza@users.noreply.github.com>
2023-01-02 11:59:31 +0100

    Update webcam.sh

--- 4-Video-surveillance/webcam.sh
+++ 4-Video-surveillance/webcam.sh
@@ -3,3 +3,6 @@ interface_gateway=eth0
 
 # Configure IP address of the interfaces
 ifconfig $interface_gateway 192.168.1.2 netmask 255.255.255.0
+
+# Configure route from gateway to Raspberry
+sudo ip route add 192.168.1.0/24 dev enp0s8
