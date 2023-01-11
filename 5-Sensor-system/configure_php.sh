062ca46ad4f0ffad9ecfdeb703a21d10cc8f7636
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-11 13:20:33 +0100

    Update configure_php.sh

--- 5-Sensor-system/configure_php.sh
+++ 5-Sensor-system/configure_php.sh
@@ -1,4 +1,5 @@
-sudo apt-get install php5.6
+sudo apt-get install php7.4
 sudo apt-get install php-mysql
-sudo apt-get install php5.6-mysql
+sudo apt-get install php7.4-mysql
 sudo service apache2 restart
+sudo service mysql restart
