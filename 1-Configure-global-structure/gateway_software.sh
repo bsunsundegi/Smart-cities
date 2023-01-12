fb945b22359f69012722e22809d963f88d6190e5
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-12 13:50:37 +0100

    Update gateway_software.sh

--- 1-Configure-global-structure/gateway_software.sh
+++ 1-Configure-global-structure/gateway_software.sh
@@ -4,3 +4,7 @@ sudo apt-get update
 # Install and enable Mosquitto
 sudo apt-get install mosquitto -y
 sudo systemctl enable mosquitto
+
+# Install hostapd and dnsmasq
+sudo apt-get install hostapd
+sudo apt-get install dnsmasq
