1df3bacdb017858fd4f4afb56c81b6d87918eca3
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-12 10:28:02 +0100

    Update sensor_selection.php

--- 5-Sensor-system/sensor_selection.php
+++ 5-Sensor-system/sensor_selection.php
@@ -24,16 +24,16 @@
 	<body>
 		<h1>City light sensors</h1>
 		<form action="sensor_data.php" method="post">
-            <label>Please, choose a sensor from the list below.</label><br>
-            <select name="sensor_id" id="sensor_id">
-            <?php
-				if(mysqli_num_rows($result) > 0) {
-					while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-						echo <option value="$row["SENSORID"]"">"$row["SENSORID"]"</option>";
-					}
-			?>
-            </select>
-            <input type="text" name="username" placeholder="Usuario">
+		<label>Please, choose a sensor from the list below.</label><br>
+		<select name="sensor_id" id="sensor_id">
+		<?php
+			if(mysqli_num_rows($result) > 0) {
+				while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+					echo <option value="$row["SENSORID"]"">"$row["SENSORID"]"</option>";
+			}
+		?>
+		</select>
+		<input type="text" name="username" placeholder="Usuario">
         </form>
 	</body>
 </html>
