e35777a01fae6535e0d13b17e9ecc80898b0af05
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-12 19:05:00 +0100

    Update server_iptables.sh

--- 1-Configure-global-structure/server_iptables.sh
+++ 1-Configure-global-structure/server_iptables.sh
@@ -1,7 +1,12 @@
 # Define variables with the name of the interfaces
 interface_gateway=enx000ec6c09235
+interface_host=ens33
 
 # Configure IP address of the interfaces
 ifconfig $interface_gateway 192.168.0.2 netmask 255.255.255.0
 
+# Delete interface ens33 as default gateway
+route del default gw 192.168.150.2 $interface_host
+
+# Set interface to PCGateway as default gateway
 route add default gw 192.168.0.1 $interface_gateway
