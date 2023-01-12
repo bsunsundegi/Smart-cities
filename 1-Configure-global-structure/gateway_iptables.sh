edcd08cabcc3b3021b4dd400edd1aad6b6be1e70
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-12 19:12:11 +0100

    Update gateway_iptables.sh

--- 1-Configure-global-structure/gateway_iptables.sh
+++ 1-Configure-global-structure/gateway_iptables.sh
@@ -3,16 +3,13 @@ interface_server=enx000ec6c091fd
 interface_webcam=enx000ec6c09233
 interface_sensors=enx000ec6c09237
 interface_wlan=wlxe894f60b829a
-interface_internet=ens33
+interface_host=ens33
 
 # Configure IP address of the interfaces
 ifconfig $interface_server 192.168.0.1 netmask 255.255.255.0
 ifconfig $interface_webcam 192.168.1.1 netmask 255.255.255.0
 ifconfig $interface_sensors 192.168.2.1 netmask 255.255.255.0
 ifconfig $interface_wlan 192.168.3.1 netmask 255.255.255.0
-
-# Configure route from gateway to server
-sudo ip route add 192.168.0.0/24 dev $interface_server
 
 # Enable IP forwarding
 echo "1" > /proc/sys/net/ipv4/ip_forward
@@ -23,4 +20,4 @@ iptables -t filter -F
 iptables -t mangle -F
 
 # Apply NAT to the traffic of the user to Internet
-iptables -t nat -A POSTROUTING -o $interface_internet -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $interface_host -j MASQUERADE
