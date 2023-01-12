297b4ff08b3a9610fbc8be080c95e67dbf0a5acc
Jose Mari Muguruza <78322990+jmuguruza@users.noreply.github.com>
2023-01-12 12:04:09 +0100

    Create suggestions.php

---
+++ 6-Suggestion-box/suggestions.php
@@ -0,0 +1,28 @@
+<?php
+    $ip = "localhost";
+	  $user = "smartcities";
+	  $pass = "tecnun";
+	  $database = "SMARTCITIES";
+	  $connection = mysqli_connect($ip, $user, $pass, $database);
+
+	  if($connection == false) {
+		  echo "Connection failed";
+	  }
+	
+    // Get the post records
+    $txtName = $_POST['txtName'];
+    $txtEmail = $_POST['txtEmail'];
+    $txtIssue = $_POST['txtIssue'];
+    $txtMessage = $_POST['txtMessage'];
+
+    // Database insert SQL code
+    $sql = "INSERT INTO `table_name` (`ID`, `Name`, `Email`, `Issue`, `Message`) VALUES ('0', '$txtName', '$txtEmail', '$txtIssue', '$txtMessage')";
+
+    // Insert in database 
+    $rs = mysqli_query($connection, $sql);
+
+    if($rs)
+    {
+	    echo "Suggestion was successfully inserted into database!";
+    }
+?>
