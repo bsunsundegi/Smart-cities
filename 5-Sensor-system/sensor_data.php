4260ae537e3d983c3e33dbd97ee1bc6f5040ec5a
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-13 17:29:24 +0100

    Update sensor_data.php

--- 5-Sensor-system/sensor_data.php
+++ 5-Sensor-system/sensor_data.php
@@ -22,7 +22,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Smart city</title>
+		<title>Sensor data</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 
