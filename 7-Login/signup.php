4a2c25e82e849314b9ae534ba18bf2d6945d1200
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-18 16:59:16 +0100

    Create signup.php

---
+++ 7-Login/signup.php
@@ -0,0 +1,36 @@
+<?php
+	$ip = "localhost";
+	$user = "smartcities";
+	$pass = "tecnun";
+	$database = "DONOSTIA";
+	$connection = mysqli_connect($ip, $user, $pass, $database);
+
+	if($connection == false) {
+		echo "Connection failed";
+	}
+
+	$name = $_POST["name"];
+	$surname = $_POST["surname"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$password_hashed = hash("sha256", $password);
+
+	if(empty($name) || empty($surname) || empty($username) || empty($password)) {
+		header("Location: signup_error_empty.html");
+	}
+	else {
+		$sql = "SELECT * FROM USERS WHERE USERNAME = '$username';";
+		$result = mysqli_query($connection, $sql);
+
+		if(mysqli_num_rows($result) == 0) {
+			$sql = "INSERT INTO USERS VALUES ('$username', '$password_hashed', '$name', '$surname', 0);";
+			$result = mysqli_query($connection, $sql);
+			if($result != 0) {
+				header("Location: signup_successful.html");
+			}
+		}
+		else {
+			header("Location: signup_error_username.html");
+		}
+	}
+?>
