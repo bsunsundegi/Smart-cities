845686321a9d4d21d9343fedda26c213ef31cf4a
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-18 17:03:03 +0100

    Update login.php

--- 7-Login/login.php
+++ 7-Login/login.php
@@ -11,12 +11,13 @@
 
 	$username = $_POST["username"];
 	$password = $_POST["password"];
+	$password_hashed = hash("sha256", $password);
 
 	if(empty($username) || empty($password)) {
 		header("Location: login_error.html");
 	}
 	else {
-		$sql = "SELECT * FROM USERS WHERE USERNAME = '$username' AND PASSWORD = '$password';";
+		$sql = "SELECT * FROM USERS WHERE USERNAME = '$username' AND PASSWORD = '$password_hashed';";
 		$result = mysqli_query($connection, $sql);
 
 		if(mysqli_num_rows($result) == 1) {
