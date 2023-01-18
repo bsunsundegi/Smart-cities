26f6efdc6fa47c9fb10564d5c3c94ab080d84855
Bruno Sunsundegi <98779297+bsunsundegi@users.noreply.github.com>
2023-01-18 19:03:21 +0100

    Update suggestions.php

--- 6-Suggestion-box/suggestions.php
+++ 6-Suggestion-box/suggestions.php
@@ -1,28 +1,50 @@
 <?php
-    $ip = "localhost";
-	  $user = "smartcities";
-	  $pass = "tecnun";
-	  $database = "SMARTCITIES";
-	  $connection = mysqli_connect($ip, $user, $pass, $database);
+	$ip = "localhost";
+	$user = "smartcities";
+	$pass = "tecnun";
+	$database = "DONOSTIA";
+	$connection = mysqli_connect($ip, $user, $pass, $database);
 
-	  if($connection == false) {
-		  echo "Connection failed";
-	  }
-	
-    // Get the post records
-    $txtName = $_POST['txtName'];
-    $txtEmail = $_POST['txtEmail'];
-    $txtIssue = $_POST['txtIssue'];
-    $txtMessage = $_POST['txtMessage'];
+	if($connection == false) {
+		echo "Connection failed";
+	}
 
-    // Database insert SQL code
-    $sql = "INSERT INTO `SUGGESTIONS` (`Name`, `Email`, `Issue`, `Message`) VALUES ('$txtName', '$txtEmail', '$txtIssue', '$txtMessage')";
+	// Get the post records
+	$username = $_POST["username"];
+	$email = $_POST["email"];
+	$issue = $_POST["issue"];
+	$message = $_POST["message"];
 
-    // Insert in database 
-    $rs = mysqli_query($connection, $sql);
+	// Database insert SQL code
+	$sql = "INSERT INTO SUGGESTIONS (USERNAME, EMAIL, ISSUE, MESSAGE) VALUES ('$username', '$email', '$issue', '$message');";
 
-    if($rs)
-    {
-	    echo "Suggestion was successfully inserted into database!";
-    }
+	// Insert in database 
+	$result = mysqli_query($connection, $sql);
 ?>
+
+<html>
+        <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="style.css">
+                <title>Suggestion registered</title>
+        </head>
+        <body>
+                <main>
+                        <div class="titulo">
+                                <h3 class="titulo">Suggestion box</h3>
+                        </div>
+                        <p>Your suggestion has been registered.</p>
+                        <form action="user_home.php" method="post">
+                                <div class="button">
+                                        <button class="button" type="submit">Send another suggestion</button>
+                                </div>
+                        </form>
+                        <form action="login.html" method="post">
+                                <div class="button">
+                                        <button class="button" type="submit">Log out</button>
+                                </div>
+                        </form>
+                </main>
+        </body>
+</html>
