5d8c8fa7046c883eabdcba2473677ad35653b576
Jose Mari Muguruza <78322990+jmuguruza@users.noreply.github.com>
2023-01-23 10:37:46 +0100

    Update gateway_iptables.sh

--- 1-Configure-global-structure/gateway_iptables.sh
+++ 1-Configure-global-structure/gateway_iptables.sh
@@ -3,7 +3,7 @@ interface_server=enx000ec6c091fd
 interface_webcam=enx000ec6c09233
 interface_sensors=enx000ec6c09237
 interface_wlan=wlxe894f60b829a
-interface_host=ens33
+interface_internet=ens33
 
 # Configure IP address of the interfaces
 ifconfig $interface_server 192.168.0.1 netmask 255.255.255.0
@@ -20,29 +20,29 @@ iptables -t filter -F
 iptables -t mangle -F
 
 # Apply NAT to the traffic of the user to Internet
-iptables -t nat -A POSTROUTING -o $interface_host -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $interface_internet -j MASQUERADE
 
 # Iptables configuration
 
 # Users to PC_Server
-iptables -t filter -A INPUT -i $interface_wlan -p http -j ACCEPT
-iptables -t filter -A OUTPUT -o $interface_wlan -p http -j ACCEPT
-iptables -t filter -A FORWARD -i $interface_wlan -o $interface_server -p http -j ACCEPT
-iptables -t filter -A FORWARD -i $interface_server -o $interface_wlan -p http -j ACCEPT
+iptables -t filter -A INPUT -i $interface_wlan -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A OUTPUT -o $interface_wlan -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_wlan -o $interface_server -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_server -o $interface_wlan -p tcp --dport 80 -j ACCEPT
 
 # Users to internet
-iptables -t filter -A FORWARD -i $interface_wlan -o $interface_host -j ACCEPT
-iptables -t filter -A FORWARD -i $interface_host -o $interface_wlan -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_wlan -o $interface_internet -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_internet -o $interface_wlan -j ACCEPT
 
 # Sensors to GT (MQTT)
 iptables -t filter -A INPUT -i $interface_sensors -j ACCEPT
-#iptables -t filter -A OUTPUT -o $interface_sensors --tcp-flags ACK -j ACCEPT		# Check if that works well
-#iptables -t filter -A OUTPUT -o $interface_sensors -j DROP
+iptables -t filter -A OUTPUT -o $interface_sensors -p tcp --tcp-flags ACK SYN -j ACCEPT		# Check if that works well
+iptables -t filter -A OUTPUT -o $interface_sensors -j DROP
 
 # Video to Server
-iptables -f filter -A FORWARD -i $interface_server -o $interface_webcam -p http -j ACCEPT
-iptables -f filter -A FORWARD -i $interface_webcam -o $interface_server -p http -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_server -o $interface_webcam -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A FORWARD -i $interface_webcam -o $interface_server -p tcp --dport 80 -j ACCEPT
 
 # Deny others
-iptables -f filter -A FORWARD -j DROP
-iptables -f filter -A INPUT -j DROP
+iptables -t filter -A FORWARD -j DROP
+iptables -t filter -A INPUT -j DROP
